e5d32b255412824ed1889c9087b6853992463146
Carver <1084024036@qq.com>
2019-05-05 23:31:37 +0800

    同步抽象类学习代码

--- basic-study/src/lesson12/Demo.java
+++ basic-study/src/lesson12/Demo.java
@@ -1,4 +1,23 @@
 package lesson12;
 
 public class Demo {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+
+        Animal animal = new Cat();
+        animal.eat();
+    }
+}
+
+abstract class Animal {
+    abstract public void eat ();
+}
+
+
+class Cat extends Animal {
+    @Override
+    public void eat () {
+        System.out.println("猫吃鱼");
+    }
 }
